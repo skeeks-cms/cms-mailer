0021144ada77855e557c6bc58b68c2420290be75
Semenov <semenov@skeeks.com>
2019-11-18 13:50:09 +0300

    dev

--- src/config/web.php
+++ src/config/web.php
@@ -6,7 +6,43 @@
  * @date 27.08.2015
  */
 return [
-    'modules' => [
+
+    'components' => [
+        'backendAdmin' => [
+            'menu' => [
+                'data' => [
+                    'other' => [
+                        'items' => [
+                            [
+                                "name"     =>['skeeks/mail', 'Mailer'],
+                                "image"       => ['\skeeks\cms\mail\assets\MailerAsset', 'icons/email.png'],
+                
+                                'items' =>
+                                [
+                                    [
+                                        "name"     =>['skeeks/mail', 'Testing sending'],
+                                        "url"       => ["mailer/admin-test"],
+                                        "image"       => ['\skeeks\cms\mail\assets\MailerAsset', 'icons/email.png'],
+                                    ],
+                
+                                    [
+                                        "name" =>['skeeks/mail', 'Settings'],
+                                        "url"   => ["cms/admin-settings", "component" => 'skeeks\cms\mail\MailerSettings'],
+                                        "image"       => ['skeeks\cms\assets\CmsAsset', 'images/icons/settings.png'],
+                                        "activeCallback"       => function($adminMenuItem) {
+                                            return (bool) (\Yii::$app->request->getUrl() == $adminMenuItem->getUrl());
+                                        },
+                                    ],
+                                ]
+                            ],
+                        ]
+                    ]
+                ],
+            ],
+        ],
+    ],
+    
+    'modules'    => [
         'mailer' => [
             'class' => 'skeeks\cms\mail\Module',
         ],
