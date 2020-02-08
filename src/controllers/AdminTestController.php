365a6006d2e16a4afe46e66b8aff76164713f10d
Semenov <semenov@skeeks.com>
2020-02-08 21:42:13 +0300

    dev

--- src/controllers/AdminTestController.php
+++ src/controllers/AdminTestController.php
@@ -5,24 +5,14 @@
  * @copyright 2010 SkeekS (СкикС)
  * @date 25.03.2015
  */
+
 namespace skeeks\cms\mail\controllers;
-use skeeks\cms\helpers\UrlHelper;
+
 use skeeks\cms\mail\models\forms\EmailConsoleForm;
-use skeeks\cms\models\Search;
-use skeeks\cms\modules\admin\actions\AdminAction;
 use skeeks\cms\modules\admin\controllers\AdminController;
 use skeeks\cms\modules\admin\controllers\helpers\rules\NoModel;
 use skeeks\cms\modules\admin\models\forms\SshConsoleForm;
 use skeeks\cms\modules\admin\widgets\ActiveForm;
-use skeeks\sx\Dir;
-use yii\data\ArrayDataProvider;
-use yii\filters\VerbFilter;
-use yii\grid\GridView;
-use yii\helpers\ArrayHelper;
-use yii\helpers\Html;
-use yii\helpers\Url;
-
-use Yii;
 use yii\web\Response;
 
 /**
@@ -42,39 +32,33 @@ class AdminTestController extends AdminController
     {
         $model = new EmailConsoleForm();
 
-        if (\Yii::$app->request->isAjax && !\Yii::$app->request->isPjax)
-        {
+        if (\Yii::$app->request->isAjax && !\Yii::$app->request->isPjax) {
             $model->load(\Yii::$app->request->post());
             \Yii::$app->response->format = Response::FORMAT_JSON;
             return ActiveForm::validate($model);
         }
 
-        $result     = "";
+        $result = "";
 
         try {
-            if ($model->load(\Yii::$app->request->post()) && $model->execute())
-            {
-                $result         = \Yii::t('skeeks/mail',"Submitted");
-            } else
-            {
-                if (\Yii::$app->request->post())
-                {
-                    $result         = \Yii::t('skeeks/mail',"Not sent");
+            if ($model->load(\Yii::$app->request->post()) && $model->execute()) {
+                $result = \Yii::t('skeeks/mail', "Submitted");
+            } else {
+                if (\Yii::$app->request->post()) {
+                    $result = \Yii::t('skeeks/mail', "Not sent");
                 }
             }
         } catch (\Exception $e) {
             //throw $e;
-            $result         = \Yii::t('skeeks/mail',"Not sent" ) . ": " . $e->getMessage();
+            $result = \Yii::t('skeeks/mail', "Not sent").": ".$e->getMessage();
         }
 
 
         return $this->render('index', [
-            'model'     => $model,
-            'result'    => $result,
+            'model'  => $model,
+            'result' => $result,
         ]);
     }
-
-
 
 
 }
